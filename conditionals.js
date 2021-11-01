24bfee0a56806fc7c80e118c6db2d35e43038222
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-01 14:07:33 -0400

    Use conditionals to tell people about the weather

--- conditionals.js
+++ conditionals.js
@@ -39,9 +39,19 @@ console.log("Now you're ready to go outside!");
 */
 //----------------------------------
 //Logical operators
+/*
 const isCitizen = true;
-const age 26;
+const age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
+}
+*/
+//-----------------------------------
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
