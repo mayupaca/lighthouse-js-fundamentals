bc072c4a93af4bc5abf4e189dc14503d72744b0c
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-02 19:04:35 -0400

    hello function

--- hello.js
+++ hello.js
@@ -6,9 +6,22 @@
 
 //-------------------------
 
-const sayHello = function (name) {
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHello("Caliban");
+// sayHello("Maranda");
+// sayHello("Ferdinand");
+
+//-----------------------
+// console.log
+const sayHelloToConsole = function (name) {
   console.log("Hello, " + name);
 }
-sayHello("Caliban");
-sayHello("Maranda");
-sayHello("Ferdinand");
+sayHelloToConsole("John");
+//------------------------
+//return
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
