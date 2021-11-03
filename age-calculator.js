1bb52de9841d52357209db95c08f56d8276a612f
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-03 10:08:15 -0400

    function practice

---
+++ age-calculator.js
@@ -0,0 +1,5 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old."
+}
+ageCalculator("Suzie, 1983, 2015");
