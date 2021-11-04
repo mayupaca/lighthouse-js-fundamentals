9f9a54a66a815d1aa217ca518444df7e12dd10e6
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-11-03 20:34:01 -0400

    array

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
+console.log("bob" || "marley");
+console.log("Jeremy" > "Khurram");
+console.log(5 * "3");
+console.log(5 + "3");
